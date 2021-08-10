3a4caa3bcdde74896bee0fd6c9de1a66d56f964d
shunlibest <shunlibest@qq.com>
2021-08-10 10:44:32 +0800

    test

--- src/test/java/com/collection/ArrayListMiniTest.java
+++ src/test/java/com/collection/ArrayListMiniTest.java
@@ -6,17 +6,9 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 class ArrayListMiniTest {
-    ArrayListMini<String> arrayListMini = new ArrayListMini<>(9);
+    ArrayListMini<String> arrayListMini = new ArrayListMini<>();
     ArrayList<String> arrayList = new ArrayList<>();
 
-    void initValues(int size) {
-        arrayListMini.clear();
-        arrayList.clear();
-        for (int i = 0; i < size; i++) {
-            arrayListMini.add("" + i);
-            arrayList.add("" + i);
-        }
-    }
 
     @Test
     void trimToSize() {
@@ -27,17 +19,53 @@ class ArrayListMiniTest {
     }
 
     @Test
-    void size() throws NoSuchFieldException, IllegalAccessException {
-        initValues(11);
+    void size()  {
+        print();
+        addValues(1);
+        print();
+
+        addValues(10);
+        print();
+
         arrayListMini.clear();
-        System.out.print("arrayListMini元素个数 " + arrayListMini.size());
-        System.out.println("arrayListMini存储空间为 " + arrayListMini.getCapacitySize());
-        System.out.print("arrayList元素个数 " + arrayList.size());
-        Class<ArrayList<String>> pClass = (Class<ArrayList<String>>) arrayList.getClass();
-        Field elementData = pClass.getDeclaredField("elementData");
-        elementData.setAccessible(true); // 为true时可以访问私有类型变量
-        Object[] o = (Object[]) elementData.get(arrayList);
-        System.out.println("arrayListMini存储空间为 " + o.length);
+        print();
+
+        arrayListMini.trimToSize();
+        print();
+
+        addValues(1);
+        print();
+
+        addValues(2);
+        print();
+
+        addValues(4);
+        print();
+    }
+
+    void print(){
+        System.out.print("arrayListMini元素个数:" + arrayListMini.size());
+        System.out.println(" 存储空间为:" + arrayListMini.getCapacitySize());
+    }
+
+
+    void addValues(int size) {
+        for (int i = 0; i < size; i++) {
+            arrayListMini.add("" + i);
+            arrayList.add("" + i);
+        }
+    }
+    private int getCapacitySize(ArrayList<String> arrayList){
+        try {
+            Class<ArrayList<String>> pClass = (Class<ArrayList<String>>) arrayList.getClass();
+            Field elementData = pClass.getDeclaredField("elementData");
+            elementData.setAccessible(true); // 为true时可以访问私有类型变量
+            Object[] o = (Object[]) elementData.get(arrayList);
+            return o.length;
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 
     @Test
